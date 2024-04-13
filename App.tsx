f1a3c10583df16c66554e79bf78856195d50c2ee
254binaryninja <139130351+254binaryninja@users.noreply.github.com>
2024-04-13 20:03:09 +0300

    Update text in App.tsx

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ function App(): React.JSX.Element {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            Edit <Text style={styles.highlight}>App.tsx</Text>Change text here to the
             screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
